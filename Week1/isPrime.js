a41b3d53bec800a8aca3976ae476da1364058131
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-12-05 14:59:23 +0330

    improved isPrime

--- Week1/isPrime.js
+++ Week1/isPrime.js
@@ -1,8 +1,6 @@
 const isPrime = (n) => {
-  for (let i = 3; i <= n / 2; i = i + 2) {
-    // 1
-    if (n % i == 0) {
-      // n/4 - 1
+  for (let i = 3; i <= n / 2; i = i + 2) { // 1
+    if (n % i == 0) { // n/4 - 1
       return false;
     }
   }
@@ -15,5 +13,23 @@ const isPrime = (n) => {
 Best Case: number 1 or 2 => O(1)
 Average Case: O(n)
 Worst Case: O(n)
-O(n) => Linear Time Complexity
+*/
+
+
+/********** Improved **********/
+const isPrime = (n) => {
+  for (let i = 3; i <= Math.sqrt(n); i = i + 2) { // 1
+    if (n % i == 0) { // n/4 - 1
+      return false;
+    }
+  }
+
+  return true; // 1
+};
+
+// T = 1 + (sqrt(n) - 1) + 1 = sqrt(n) + 1
+/*
+Best Case: number 1 or 2 => O(1)
+Average Case Improved: O(sqrt(n))
+Worst Case Improved: O(sqrt(n))
 */
