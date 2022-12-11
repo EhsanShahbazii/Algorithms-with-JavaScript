4a4b883957c71d90c7b0de0f745656f83c850b42
L4st Surviv0r <ehsan.shahbazicode@gmail.com>
2022-12-11 23:49:41 +0330

    add binarySearch

---
+++ Week2/binarySearch.js
@@ -0,0 +1,11 @@
+Array.prototype.br_search = function (target) {
+  var half = parseInt(this.length / 2);
+  if (target === this[half]) {
+    return half;
+  }
+  if (target > this[half]) {
+    return half + this.slice(half,this.length).br_search(target);
+  } else {
+    return this.slice(0, half).br_search(target);
+  }
+};
