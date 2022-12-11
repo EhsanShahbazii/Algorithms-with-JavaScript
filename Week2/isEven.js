8dbc26ce8b1cf843ab974dd3912b2069dc1e1d93
L4st Surviv0r <ehsan.shahbazicode@gmail.com>
2022-12-11 23:48:37 +0330

    add isEven

---
+++ Week2/isEven.js
@@ -0,0 +1,14 @@
+const is_even_recursion = (number) => {
+  if (number < 0) {
+    number = Math.abs(number);
+  }
+  if (number===0) {
+    return true;
+  }
+  if (number===1) {
+    return false;
+  } else {
+    number = number - 2;
+    return is_even_recursion(number);
+  }
+}
