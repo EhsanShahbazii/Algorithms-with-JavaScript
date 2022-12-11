06fd6b5cfd381a3d21530fe7756973ccf0450734
L4st Surviv0r <ehsan.shahbazicode@gmail.com>
2022-12-11 23:47:17 +0330

    add fibonacciSeries

---
+++ Week2/fibonacciSeries.js
@@ -0,0 +1,9 @@
+const fibonacci_series = (n) => {
+  if (n===1) {
+    return [0, 1];
+  } else {
+    var s = fibonacci_series(n - 1);
+    s.push(s[s.length - 1] + s[s.length - 2]);
+    return s;
+  }
+};
