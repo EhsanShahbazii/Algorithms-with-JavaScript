69ab582906b814882c74e883263a50b19e363974
L4st Surviv0r <ehsan.shahbazicode@gmail.com>
2022-12-30 21:21:12 +0330

    Create nthroot.js

---
+++ nthroot.js
@@ -0,0 +1,12 @@
+/* Write a JavaScript function to calculate the nth root of a number. */
+
+function nthroot(x, n) {
+  ng = n % 2;
+  if((ng == 1) || x<0)
+    x = -x;
+  var r = Math.pow(x, 1 / n);
+  n = Math.pow(r, n);
+  
+  if(Math.abs(x - n) < 1 && (x > 0 === n > 0))
+    return ng ? -r : r; 
+}
