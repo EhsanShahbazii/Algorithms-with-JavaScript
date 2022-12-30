211248d73dcb604781dc0e8202f2225c6cc82a31
L4st Surviv0r <ehsan.shahbazicode@gmail.com>
2022-12-30 21:24:44 +0330

    Create TSeparators.js

---
+++ TSeparators.js
@@ -0,0 +1,7 @@
+/* Write a JavaScript function to print an integer with commas as thousands separators. */
+
+function thousands_separators(num) {
+    var num_parts = num.toString().split(".");
+    num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return num_parts.join(".");
+ }
