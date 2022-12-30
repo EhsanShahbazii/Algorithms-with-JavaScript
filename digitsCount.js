b90c70f9a14ee29925a1209e15e20ee09bad96be
L4st Surviv0r <ehsan.shahbazicode@gmail.com>
2022-12-30 21:25:54 +0330

    Create digitsCount.js

---
+++ digitsCount.js
@@ -0,0 +1,13 @@
+/* Write a JavaScript function to count the digits of an integer. */
+
+function digits_count(n) {
+  var count = 0;
+  if (n >= 1) ++count;
+
+  while (n / 10 >= 1) {
+    n /= 10;
+    ++count;
+  }
+
+  return count;
+}
